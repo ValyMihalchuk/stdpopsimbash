486d11d70e3aab01cd466b7ed9fc7d1803fe17db
Valy Mihalchuk <valy378@gmail.com>
2022-05-09 19:55:06 +0300

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-while read -r line; do stdpopsim DroMel -g ComeronCrossover_dm6 -c "$line" -o "$line".ts -d OutOfAfrica_2L06 2 4; done
+while read -r line; do stdpopsim DroMel -g ComeronCrossover_dm6 -c "$line" -o "$line".ts -d OutOfAfrica_2L06 4 4; done
